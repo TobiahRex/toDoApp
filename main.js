a7ddb41f52b37cf5dd72bba2f47606d0cc70995d
RexCorpDev <rexcorpfx@gmail.com>
2016-06-11 18:00:22 -0700

    almost finished with toggle button.

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function init(){
   renderTodos(dbTodos);
   $('.add-todo').on('click', newTodo);
   $('tbody').on('click', '.delete-todo',deleteTodo);
+  $('tbody').on('click', '.toggle-group', isCompleted);
 };
 function getTodos(){
   var todosStr = localStorage.todos;
@@ -30,15 +31,13 @@ function newTodo(){
   $('.cancel-new-todo').on('click', cancelTodo);
 
 };
-
 function saveTodo(){
-console.log('save todo');
   let dbTodos = getTodos();
   let newTaskObj = {
     index     : dbTodos.length + 1,
     task      : $('input.new-task').val(),
     dueDate   : $('input.new-due-date').val(),
-    complete  : false
+    completed : false
   };
   $('input.new-task').val('');
   $('input.new-due-date').val(''),
@@ -53,16 +52,17 @@ function cancelTodo(){
   $('input.new-due-date').val(''),
   $('.new-todo').addClass('hidden');
 };
-
 function renderTodos(todos){
-    let $todos = [];
-    todos.map(todo => {
+  let $todos = [];
+  todos.map(todo => {
     let $tr = $('.template').clone();
     $tr.removeClass('template');
     $tr.addClass('task');
     $tr.find('.index').text(todo.index);
     $tr.find('.task').text(todo.task);
     $tr.find('.due-date').text(todo.dueDate);
+    debugger;
+    !todo.completed ? $tr.find('input.is-complete').bootstrapToggle('on') : $tr.find('input.is-complete').bootstrapToggle('off') ;
     return $todos.push($tr);
   });
   let newTodoClone = $('tr.new-todo').clone();
@@ -70,7 +70,6 @@ function renderTodos(todos){
   $('tbody').empty();
   $('tbody').append(newTodoClone).append(templateClone).append($todos);
 };
-
 function deleteTodo(){
   var dbTodos = getTodos();
   let $index = $(this).parent().parent().index();  // +2 for Dom Index. -2 for localStorage index.
@@ -79,4 +78,21 @@ function deleteTodo(){
   dbTodos.splice(index, 1);
   writeTodo(dbTodos);
   renderTodos(dbTodos);
+};
+
+function isCompleted(){
+  console.log('completed');
+  let dbTodos = getTodos();
+  let $index = $(this).parent().parent().parent().index();
+  let index = $index-2;
+
+  let $state =  $(this).parent().parent().find('input').prop('checked');  // false for new completion.
+  if(!$state === false) return;                                           // if It's already been completed exit function.
+  $(this).parent().parent().find('input').prop('checked', true);          // if not, then change it to completed.
+  dbTodos[index].completed = true;                                        // change completed status in LocalStorage.
+  writeTodo(dbTodos);                                                     // saving to local stoarge.
+  $(this).parent().parent().find('input').bootstrapToggle('off');         // update the current render in the table.  (off === completed. on === incomplete.)
+
+  // $(this).bootstrapToggle('toggle');
+
 };
