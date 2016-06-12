9c25df2c78c7f3d1f133a85f444a68d810cbd36a
RexCorpDev <rexcorpfx@gmail.com>
2016-06-11 23:04:46 -0700

    going to try to re-order renderTodos for fix

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
 'use strict';
 
-// TODO
-
 $(document).ready(init);
 
 function init(){
   var dbTodos = getTodos();
   renderTodos(dbTodos);
   $('.add-todo').on('click', newTodo);
-  $('tbody').on('click', '.delete-todo',deleteTodo);
-  $('tbody').on('click', 'tr.task', isCompleted);
-};
-function getTodos(){
-  var todosStr = localStorage.todos;
-  var dbTodos;
-  try {
-    dbTodos = JSON.parse(todosStr);
-  } catch(err) {
-    dbTodos = [];
-  };
-  return dbTodos; // array
-};
-function writeTodo(todos){
-  let newTodos = JSON.stringify(todos);
-  localStorage.todos = newTodos;
+  $('tbody').on('click', '.delete-todo', deleteTodo);
+  $('tbody').on('click', '.is-complete', isCompleted);
 };
 function newTodo(){
   $('.new-todo').removeClass('hidden'); // shows the new-todo row
@@ -38,10 +22,11 @@ function saveNewTodo(){
     dueDate   : $('input.new-due-date').val(),
     completed : false
   };
+  dbTodos.push(newTaskObj);
   $('input.new-task').val('');
   $('input.new-due-date').val(''),
   $('.new-todo').addClass('hidden');
-  dbTodos.push(newTaskObj);
+
   writeTodo(dbTodos);
   renderTodos(dbTodos);
 };
@@ -52,23 +37,34 @@ function cancelTodo(){
   $('.new-todo').addClass('hidden');
 };
 function renderTodos(todos){
-  let $todos = [];
-  todos.map(todo => {
+  let $todos = todos.map(todo => {
     let $tr = $('.template').clone();
     $tr.removeClass('template');
-    $tr.addClass('task');
+    $tr.addClass('todo');
     $tr.find('.index').text(todo.index);
     $tr.find('.task').text(todo.task);
     $tr.find('.due-date').text(todo.dueDate);
     $tr.find('input').data('completed', todo.completed);
-
-    // the above line determines whether to render the todo as complete or incomplete, and then saves that data in element.
-    return $todos.push($tr);
+    return $tr;
+    let newTodoClo ty();
   });
-  let newTodoClone = $('tr.new-todo').clone();
-  let templateClone = $('tr.template').clone();
-  $('tbody').empty();
   $('tbody').append(newTodoClone).append(templateClone).append($todos);
+};
+
+function isCompleted(){
+  let dbTodos = getTodos();
+  let $index = $(this).parent().index();
+  let index = $index-2;
+  let $state = $('tr.todo').find('input').data('completed');
+  let newState = !$state;
+  let on_off = '';
+
+  newState ? on_off = 'off' : on_off = 'on';
+
+  $(this).find('input').bootstrapToggle(on_off);
+  dbTodos[index].completed = newState;
+  writeTodo(dbTodos);
+  renderTodos(dbTodos);
 };
 function deleteTodo(){
   var dbTodos = getTodos();
@@ -79,20 +75,17 @@ function deleteTodo(){
   writeTodo(dbTodos);
   renderTodos(dbTodos);
 };
-
-function isCompleted(){
-  let dbTodos = getTodos();
-  let $index = $(this).index();
-  let index = $index-2;
-  let $state = $(this).find('input').data('completed');
-  let newState = !$state;                                                           // toggle $state.
-  let on_off = '';
-
-  newState ? on_off = 'on' : on_off = 'off';
-
-  $(this).find('input').bootstrapToggle(on_off);
-  dbTodos[index].completed = newState;                                          // change completed status in LocalStorage.
-  writeTodo(dbTodos);                                                         // saving to local stoarge.
-
-
+function getTodos(){
+  var todosStr = localStorage.todos;
+  var dbTodos;
+  try {
+    dbTodos = JSON.parse(todosStr);
+  } catch(err) {
+    dbTodos = [];
+  };
+  return dbTodos; // array
+};
+function writeTodo(todos){
+  let newTodos = JSON.stringify(todos);
+  localStorage.todos = newTodos;
 };
